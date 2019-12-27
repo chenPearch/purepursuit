08c4df1879f2c04dd1653631ba1250bd4f300a0c
chenPearch <45401724+chenPearch@users.noreply.github.com>
2019-12-27 13:34:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# purepursuit
+ this is a purePursuit simulator created by the FRC 4586 team
---
+++ src/main/java/Utils/MappingConstants.java
@@ -0,0 +1,6 @@
+package Utils;
+
+public class MappingConstants {
+    public static final double fieldWidth = Units.feetToMeters(54);
+    public static final double fieldHeight = Units.feetToMeters(27);
+}
